4d7b8d4b7018369a797620b88a20c7472a20df63
unknown <Administrator@Asiet-Lab.ASIETDomain.com>
2019-01-17 16:26:14 +0530

    day2

---
+++ HTMLCSS/script1.js
@@ -0,0 +1,17 @@
+var a ="Nikhil";
+console.log(a);
+var b=2;
+var c=3;
+var d=b**c;
+console.log(d);
+var e=10;
+f=e++;
+console.log(e++);
+const i=15;
+
+console.log(i);
+var x=10;
+if(x%2==0)
+{console.log("X is even");}
+else
+{console.log("x is odd");}
