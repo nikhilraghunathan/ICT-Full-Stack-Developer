f1dd029b46f99d024a52309d94ef2c9214c084b6
unknown <Administrator@Asiet-Lab.ASIETDomain.com>
2019-01-22 16:32:22 +0530

    python day1

--- python/day1/function.py
+++ python/day1/function.py
@@ -2,5 +2,16 @@ def printme(str):
     print(str)
     return
 
-printme("first call to printme")
-printme("second cal to printme")
+printme("first call to printt")
+printme("second cal to printt")
+
+def printt(a,b):
+    c=a*b
+    c1=a+b
+    c2=a-b
+    return(c,c1,c2)
+x=int(input("Enter 1st no :"))
+y=int(input("enter 2nd no :"))
+z=printt(x,y)
+print("Outside ",z)
+print(type(z))
