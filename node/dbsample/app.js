c2d6809b57a9f4406f67d145bc7923b5fbc7778c
unknown <Administrator@Asiet-Lab.ASIETDomain.com>
2019-02-05 16:33:15 +0530

    node day 4

--- node/dbsample/app.js
+++ node/dbsample/app.js
@@ -4,17 +4,26 @@ var mongodb = require("mongodb");
 var MongoClient = mongodb.MongoClient;
 var url = "mongodb://127.0.0.1:27017/nikdb";
 
+var bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended:true}));
+
 app.set('view engine', 'ejs');
 
 app.get("/",function(req,res){
     res.render("home");
 })
-
-app.get("/insert",function(req,res){
+// ---------------------------------------------------------------------------------------------------------------------- //
+app.post("/insert",function(req,res){
     MongoClient.connect(url,function(err,database){
+        var txtname = req.body.name;
+        var txtsal = req.body.salary;
+
+        // res.send(txtname);
+
         var datab = database.db('nikdb');    //  connecting database to variable
         var collectn = datab.collection('employee1');        // linking table employee1 to variable 'collections'
-        var data = {eid:"e0021",name:"RAMAN",dept:"it",salary:"11000",loc:"kkkk"};
+        var data = {eid:"e007",ename:txtname,dept:"it",salary:txtsal,loc:"kkkk"};
+
         collectn.insert(data, function(err,result){
             if(err){ res.send("something went wrong"); }
             else{ res.send("data inserted successfully"); }
@@ -23,7 +32,113 @@ app.get("/insert",function(req,res){
     })
 
 })
+// ---------------------------------------------------------------------------------------------------------------------- //
+app.get("/view",function(req,res){
+    MongoClient.connect(url,function(err,database) {
+        if(err){ res.send("something went wrong"); }
+        else{
+            var datab = database.db("nikdb");
+            var collectn = datab.collection("employee1");
+            collectn.find().toArray(function(err,tbdata){
+                if(err){ res.send("Error"); }
+                else{  res.render('empview',{data:tbdata}); }
+            })
+        }
+    })
+})
+// ---------------------------------------------------------------------------------------------------------------------- //
 
+app.get("/delete",function(req,res){
+    MongoClient.connect(url,function(err,database) {
+        if(err){ res.send("something went wrong"); }
+        else{
+            var datab = database.db("nikdb");
+            var collectn = datab.collection("employee1");
+            collectn.find().toArray(function(err,tbdata){
+                if(err){ res.send("Error"); }
+                else{  res.render('empdelete',{data:tbdata}); }
+            })
+        }
+    })
+})
+// ---------------------------------------------------------------------------------------------------------------------- //
+        app.post("/deleted",function(req,res){
+            MongoClient.connect(url,function(err,database) {
+                if(err){ res.send("something went wrong"); }
+                else{
+                    var enamee = req.body.employ;
+                    if(enamee== '')
+                    { enamee=null; }
+                    var datab = database.db("nikdb");
+                    var collectn = datab.collection("employee1");
+                    var query = {ename:enamee };
+                    collectn.remove(query,function(err,obj){
+                        if(err){ throw err; }
+                        else{  res.send("Employee deleted"); }
+                    })
+                }
+            })
+        })
+
+// ---------------------------------------------------------------------------------------------------------------------- //
+
+app.get("/edit",function(req,res){
+    MongoClient.connect(url,function(err,database) {
+        if(err){ res.send("something went wrong"); }
+        else{
+            var datab = database.db("nikdb");
+            var collectn = datab.collection("employee1");
+            collectn.find().toArray(function(err,tbdata){
+                if(err){ res.send("Error"); }
+                else{  res.render('empedit',{data:tbdata}); }
+            })
+        }
+    })
+})
+// ---------------------------------------------------------------------------------------------------------------------- //
+        app.post("/editing",function(req,res){
+            MongoClient.connect(url,function(err,database) {
+                var enamee = req.body.employedit;
+                var query = {ename:enamee };
+                if(err){ res.send("something went wrong"); }
+                else{
+                    var datab = database.db("nikdb");
+                    var collectn = datab.collection("employee1");
+                    collectn.find(query).toArray(function(err,tbdata){
+                        if(err){ res.send("Error"); }
+                        else{  res.render('empedit1',{data:tbdata}); }
+                    })
+                }
+            })
+        })
+
+// ---------------------------------------------------------------------------------------------------------------------- //
+        app.post("/edited",function(req,res){
+            MongoClient.connect(url,function(err,database) {
+                var b_eid = req.body.a_eid;
+                var b_ename = req.body.a_ename;
+                var b_dept = req.body.a_dept;
+                var b_salary = req.body.a_salary;
+                var b_loc = req.body.a_loc;
+                var b_dob = req.body.a_dob;
+
+                var the_id = {eid:b_eid};
+                var values = {$set:{ename:b_ename,dept:b_dept,salary:b_salary,loc:b_loc,dob:b_dob}};
+                
+                if(err){ res.send("something went wrong"); }
+                else{
+                    var datab = database.db("nikdb");
+                    var collectn = datab.collection("employee1");
+
+                    collectn.updateOne(the_id, values, function(err,result){
+                        if(err){ res.send("Error"); }
+                        else{ res.send("Data updated successfully"); }
+                    })
+                }
+            })
+        })
+
+// ---------------------------------------------------------------------------------------------------------------------- //
 app.listen(8000,function(req,res){
-    console.log("server is listening")
+    console.log("server is listening");
 });
---
+++ node/heroku1/app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+const app = express();
+
+var port = preocess.env.PORT||2255;
+app.listen(port,function(req,res){
+    console.log("server running");
+})
